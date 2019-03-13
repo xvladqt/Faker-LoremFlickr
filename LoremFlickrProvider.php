aae391fb0e2c2559976e200d2cce386b7f3cd847
Xvlad (Vladyslav Volynets) <Xvladxtreme@gmail.com>
2019-03-13 11:17:20 +0200

    Push

---
+++ LoremFlickrProvider.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace Xvladqt\Faker;
+
+use Faker\Provider\Base as BaseProvider;
+
+/**
+ * loremflickr.com provider for Faker
+ *
+ * @author Vladyslav Volynets <xvladxtreme@gmail.com>
+ *
+ * @link http://loremflickr.com/
+ */
+class LoremFlickrProvider extends BaseProvider
+{
+
+    /**
+     * Generate the URL that will return a random image
+     *
+     * Set randomize to false to remove the random GET parameter at the end of the url.
+     *
+     * @example 'https://loremflickr.com/640/480?random=1'
+     *
+     * @param integer $width
+     * @param integer $height
+     * @param array|null $keywords
+     * @param bool $randomize
+     * @param bool $gray
+     *
+     * @return string
+     */
+    public static function imageUrl($width = 640, $height = 480, $keywords = null, $randomize = true, $gray = false)
+    {
+        $baseUrl = "https://loremflickr.com";
+        $url = '';
+        if ($gray) {
+            $url .= "/g";
+        }
+
+        $url .= "/{$width}/{$height}";
+
+        if (is_array($keywords) && count($keywords)) {
+            $url .= '/'.implode(',', keywords);
+        }
+
+        if ($randomize) {
+            $url .= '?random=' . static::randomNumber(5, true);
+        }
+
+        return $baseUrl . $url;
+    }
+
+    /**
+     * Download a remote random image to disk and return its location
+     *
+     * Requires curl, or allow_url_fopen to be on in php.ini.
+     *
+     * @example '/path/to/dir/13b73edae8443990be1aa8f1a483bc27.jpg'
+     */
+    public static function image($dir = null, $width = 640, $height = 480, $keywords = null, $fullPath = true, $randomize = true, $gray = false)
+    {
+        $dir = is_null($dir) ? sys_get_temp_dir() : $dir; // GNU/Linux / OS X / Windows compatible
+        // Validate directory path
+        if (!is_dir($dir) || !is_writable($dir)) {
+            throw new \InvalidArgumentException(sprintf('Cannot write to directory "%s"', $dir));
+        }
+
+        // Generate a random filename. Use the server address so that a file
+        // generated at the same time on a different server won't have a collision.
+        $name = md5(uniqid(empty($_SERVER['SERVER_ADDR']) ? '' : $_SERVER['SERVER_ADDR'], true));
+        $filename = $name .'.jpg';
+        $filepath = $dir . DIRECTORY_SEPARATOR . $filename;
+
+        $url = static::imageUrl($width, $height, $keywords, $randomize, $gray);
+
+        // save file
+        if (function_exists('curl_exec')) {
+            // use cURL
+            $fp = fopen($filepath, 'w');
+            $ch = curl_init($url);
+            curl_setopt($ch, CURLOPT_FILE, $fp);
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+            $success = curl_exec($ch) && curl_getinfo($ch, CURLINFO_HTTP_CODE) === 200;
+            fclose($fp);
+            curl_close($ch);
+
+            if (!$success) {
+                unlink($filepath);
+
+                // could not contact the distant URL or HTTP error - fail silently.
+                return false;
+            }
+        } elseif (ini_get('allow_url_fopen')) {
+            // use remote fopen() via copy()
+            $success = copy($url, $filepath);
+        } else {
+            return new \RuntimeException('The image formatter downloads an image from a remote HTTP server. Therefore, it requires that PHP can request remote hosts, either via cURL or fopen()');
+        }
+
+        return $fullPath ? $filepath : $filename;
+    }
+
+}
+?>
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# Faker-LoremFlickr
+Faker-LoremFlickr
+===============================
+
 loremflickr provider for Faker
+[loremflickr.com](http://loremflickr.com/) provider for [Faker](https://github.com/fzaninotto/Faker).
+
+## Install
+
+Install the LoremFlickr Provider by adding `xvladqt/faker-lorem-flickr` to your composer.json or from CLI:
+
+```
+$ composer require xvladqt/faker-lorem-flickr
+```
+
+## Usage
